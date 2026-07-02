d0a939776e91437139bbcc080d6fc6d6d599a837
efzo <ojomoefosa@gmail.com>
2026-07-02 00:14:55 -0500

    Added Date time to the model

--- schemas.py
+++ schemas.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from datetime import datetime, UTC
 
 
 class TaskCreate(BaseModel):
     title: str = Field(..., min_length=1, max_length=120)
     is_done:bool = False
+    current_time: datetime = Field(default_factory=datetime.now(UTC))
     
     
 class TaskResponse(BaseModel):
-    id:int
+    id: int
     title:str
     is_done: bool
+    current_time: datetime = Field(default_factory=datetime.now(UTC))
